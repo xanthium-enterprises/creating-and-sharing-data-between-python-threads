80e9fe8712045beb427312a8f97adac9951b33cb
xanthium-enterprises <contact@xanthium.in>
2024-02-18 16:46:46 +0530

    added a file

---
+++ _1_python_threads.py
@@ -0,0 +1,13 @@
+import time
+import threading
+
+def do_something():
+    print(f'\nEntered the function do_something(){threading.current_thread()}')
+    time.sleep(1)
+    print('\nDone Sleeping')
+    
+print(f'\nStart of Main Thread {threading.current_thread() }')
+t1 = threading.Thread(target = do_something)
+t1.start()                                     #start the threads
+
+print('\nEnd of Main Thread\n+---------------------------+')
