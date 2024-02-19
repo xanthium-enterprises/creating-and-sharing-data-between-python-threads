f19971a69884881b2518271d1bdedaf6b6f00b73
xanthium-enterprises <contact@xanthium.in>
2024-02-19 08:24:02 +0530

    added files

---
+++ _1_create_python_threads_no_join.py
@@ -0,0 +1,22 @@
+'''
+Creating Threads in Python without join
+
+(c) www.xanthium.in
+
+'''
+import time
+import threading   #required for threading 
+
+
+def do_something():
+    print(f'\nEntered do_something() ')
+    time.sleep(2)
+    print('\nDone Sleeping in thread\n')
+
+    
+print(f'\nStart of Main Thread ')
+
+t1 = threading.Thread(target = do_something)   #create the thread t1
+t1.start()                                     #start the threads
+
+print('\nEnd of Main Thread\n+---------------------------+')
