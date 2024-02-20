563d5bdc3f7894a88c8475235d08ed949a0ebefe
xanthium-enterprises <contact@xanthium.in>
2024-02-20 14:45:18 +0530

    added files

--- _4_return_values_from_python_thread_global_var.py
+++ _4_return_values_from_python_thread_global_var.py
@@ -8,8 +8,10 @@ import time
 import threading   #required for threading 
 
 def add_two_numbers(no1,no2):
-    global global_sum
+
+    global global_sum       # tell that you want to access the global variable 
     global_sum = no1 + no2
+
     time.sleep(2)
     
 
@@ -20,7 +22,7 @@ global_sum = 0  # Global Value used to return data from thread
 print(f'Sum Intial Value -> {global_sum}')
 
 
-First_No  = 5
+First_No  = 20
 Second_No = 10
 
 t1 = threading.Thread(target = add_two_numbers,args=(First_No,Second_No))  # create the thread t1
