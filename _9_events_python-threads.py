3a47bdde5d7fb174b22e5f3f1d69beb4c41e9bc7
xanthium-enterprises <contact@xanthium.in>
2024-02-23 11:04:55 +0530

    Update _9_events_python-threads.py

--- _9_events_python-threads.py
+++ _9_events_python-threads.py
@@ -14,7 +14,7 @@ def function_t():
 
     print('Entered t1 thread func')
 
-    my_event.wait()      # wait for the event to be set in main thread
+    my_event.wait(timeout = 10)      # wait for the event to be set in main thread
 
     print('Event is set,so this line gets printed')
     
@@ -27,7 +27,7 @@ t1.start()                                 # start  t1 thread
 print('will set the event in 5 seconds')
 
 time.sleep(5)    #wait 5 seconds 
-my_event.set()   #set the event after 5 seconds
+#my_event.set()   #set the event after 5 seconds
 
 t1.join()
 
